68eb6aa83cb3f1e89572d69bd629a6329cb8de58
wilkin547 <wilkintrinidad10@gmail.com>
2021-03-30 14:03:48 -0900

    comienzo del proyecto

---
+++ PadreGameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadreGameController : MonoBehaviour
+{
+    private short points;
+    public short Points => points;
+
+    private bool fail;
+    public bool Fail => fail;
+    
+        
+        
+    
+
+
+    private void Start()
+    {
+       
+        Debug.Log(Points);
+    }
+
+
+}
