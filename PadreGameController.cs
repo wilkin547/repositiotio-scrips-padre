eb4bfd5838c4eb362f1f2b1733be0fc985691a7d
wilkin547 <wilkintrinidad10@gmail.com>
2021-03-30 17:12:32 -0900

    aqui ya declare todas las variables y metodos comunes de la clase padre

--- PadreGameController.cs
+++ PadreGameController.cs
@@ -1,23 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PadreGameController : MonoBehaviour
 {
+    [SerializeField]
+    private bool Active60Frames;
     private short points;
     public short Points => points;
 
     private bool fail;
     public bool Fail => fail;
-    
-        
-        
-    
+
+    [SerializeField]
+    [Range(0, 1000)]
+    private float speed;
+    public float Speed => speed;
+
+    [SerializeField]
+    private TMP_Text Score;
+    [SerializeField]
+    private GameObject gameOver;
+
+    virtual public void UpdateScore()
+    {
+        points++;
+        Score.text = "Score: " + Points;
+    }
+
+    public void GameOver()
+    {
+        fail = true;
+        gameOver.SetActive(true);
+    }
 
 
     private void Start()
     {
-       
+        if (Active60Frames)
+        {
+            Application.targetFrameRate = 60;
+        }
+        else
+        {
+            Application.targetFrameRate = 30;
+        }
         Debug.Log(Points);
     }
 
